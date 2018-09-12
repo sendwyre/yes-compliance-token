5f5968b903f1d864e4fe06700134052280592a84
Tyson Malchow <tyson.malchow@gmail.com>
2018-09-11 23:38:24 -0500

    typo

--- contracts/WyreYesComplianceToken.sol
+++ contracts/WyreYesComplianceToken.sol
@@ -1,8 +1,8 @@
 pragma solidity ^0.4.24;
 
-import "./YesComplianceTokenV1.sol";
+import "./YesComplianceTokenImpl.sol";
 
-contract WyreYesComplianceToken is YesComplianceTokenV1 {
+contract WyreYesComplianceToken is YesComplianceTokenImpl {
 
     constructor() public YesComplianceTokenV1('WYRE-YES', 'Wyre YES Compliance Token') {
 
